04825edd57935b22b63e19b360cdb38325f7ce10
Kimgmltn <Kimgmltn0323@gmail.com>
2025-11-25 23:37:26 +0900

    상속을 통한 커넥션 관심사 분리

---
+++ src/ch03/springbook/user/dao/DUserDao.java
@@ -0,0 +1,15 @@
+package ch03.springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DUserDao extends UserDao{
+
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "heesu", null);
+        return c;
+    }
+}
