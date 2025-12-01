db6da3f884d30c521cc35291b531031b520888ba
Kimgmltn <Kimgmltn0323@gmail.com>
2025-12-02 00:39:40 +0900

    junit을 이용한 테스트 예시

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -1,16 +1,36 @@
 package springbook.user.dao;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException,SQLException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao userDao = context.getBean("userDao", UserDao.class);
-        User user = userDao.get("test");
-        System.out.println(user.getName());
+        JUnitCore.main("springbook.user.dao.UserDaoTest");
+    }
+
+    @Test
+    public void addAndGet() throws ClassNotFoundException, SQLException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User();
+        user1.setId("test1");
+        user1.setName("test1");
+        user1.setPassword("test1");
+
+        dao.add(user1);
+
+        User user2 = dao.get(user1.getId());
+
+        Assert.assertThat(user2.getName(), CoreMatchers.is(user1.getName()));
+        Assert.assertThat(user2.getPassword(), CoreMatchers.is(user1.getPassword()));
     }
 }
