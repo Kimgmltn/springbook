8a1aa7e370cad91b194f31bfb0460baca05b392d
Kimgmltn <Kimgmltn0323@gmail.com>
2025-12-02 13:10:13 +0900

    addAndGet() 테스트 보완

--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -30,21 +30,24 @@ public class UserDaoTest {
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        User user1 = new User("test1", "test1", "test1");
+        User user2 = new User("test2", "test2", "test2");
+
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
-        User user1 = new User();
-        user1.setId("test1");
-        user1.setName("test1");
-        user1.setPassword("test1");
-
         dao.add(user1);
-        assertThat(dao.getCount(), is(1));
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        User user2 = dao.get(user1.getId());
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
 
-        assertThat(user2.getName(), is(user1.getName()));
-        assertThat(user2.getPassword(), is(user1.getPassword()));
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
+
     }
 
     @Test
