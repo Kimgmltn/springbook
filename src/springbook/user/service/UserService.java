9c0aea3f8eb29888ce4c2ca45aceb70d842c801c
Kimgmltn <Kimgmltn0323@gmail.com>
2025-12-25 12:05:47 +0900

    비즈니스로직을 메소드로 추출

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -35,16 +35,20 @@ public class UserService {
         TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
 
         try{
-            List<User> users = userDao.getAll();
-            for (User user : users) {
-                if(canChangeLevel(user)){
-                    upgradeLevel(user);
-                }
-            }
+            upgradeLevelsInternal();
             this.transactionManager.commit(status);
         }catch(Exception e){
             this.transactionManager.rollback(status);
             throw e;
+        }
+    }
+
+    private void upgradeLevelsInternal() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            if(canChangeLevel(user)){
+                upgradeLevel(user);
+            }
         }
     }
 
