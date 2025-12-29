dc02e97639223c95d30631945facd3c5e06a228b
Kimgmltn <Kimgmltn0323@gmail.com>
2025-12-29 20:29:04 +0900

    TransactionHandler를 이용한 트랜잭션 처리

--- src/springbook/user/service/UserServiceImplTest.java
+++ src/springbook/user/service/UserServiceImplTest.java
@@ -17,6 +17,7 @@ import springbook.user.dao.Level;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -117,14 +118,18 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void upgradeAllOrNothing(){
+    public void upgradeAllOrNothing() throws Exception{
         TestUserServiceImpl testUserService = new TestUserServiceImpl(users.get(3).getId());
         testUserService.setUserDao(this.userDao);
         testUserService.setMailSender(this.mailSender);
 
-        UserServiceTx txUserService = new UserServiceTx();
-        txUserService.setTransactionManager(transactionManager);
-        txUserService.setUserService(testUserService);
+        TransactionHandler txHandler = new TransactionHandler();
+        txHandler.setTarget(testUserService);
+        txHandler.setTransactionManager(this.transactionManager);
+        txHandler.setPattern("upgradeLevels");
+        UserService txUserService = (UserService) Proxy.newProxyInstance(
+                getClass().getClassLoader(), new Class[]{UserService.class}, txHandler
+        );
 
         userDao.deleteAll();
 
