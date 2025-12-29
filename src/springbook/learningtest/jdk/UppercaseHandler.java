19fca694fb562ca29cc0e7befef6dc5ce21c5e76
Kimgmltn <Kimgmltn0323@gmail.com>
2025-12-29 11:04:12 +0900

    확장된 uppdercaseHandler -Object타입으로 받고, 필요한거만 uppercase하도록 적용

--- src/springbook/learningtest/jdk/UppercaseHandler.java
+++ src/springbook/learningtest/jdk/UppercaseHandler.java
@@ -4,18 +4,30 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler {
-    Hello target;
+    Object target;
 
-    //위임해야 하기 때문에 타깃 오브젝트를 주입받아 둔다.
-    public UppercaseHandler(Hello target) {
+    //위임해야 하기 때문에 타깃 오브젝트를 주입Object받아 둔다.
+    //어떤 종류의 인터페이스를 구현한 타겟이도 적용 가능하도록 Object타입으로 수정
+    public UppercaseHandler(Object target) {
         this.target = target;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         //타깃으로 위임. 인터페이스의 메소드 호출에 모두 적용.
-        // (String)은 Hello객체의 모든 return값이 String 이므로 검증없이 바로 String으로 형변환 함
-        String ret = (String)method.invoke(target, args);
-        return ret.toUpperCase();
+        Object ret = method.invoke(target, args);
+        //호출한 메소드의 return값이 String일 경우에만 대문자 변경 가능하도록 수정
+//        if(ret instanceof String){
+//            return ((String)ret).toUpperCase();
+//        }else{
+//            return ret;
+//        }
+
+        //리턴타입과 메소드 이름이 일치하는 경우에만 부가기능을 적용한다.
+        if(ret instanceof String && method.getName().startsWith("say")) {
+            return ((String) ret).toUpperCase();
+        }else{
+            return ret;
+        }
     }
 }
