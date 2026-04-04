7a71434378af0806c456ced8718b7fdead33bcc2
khs <T1900843@partner.tmapmobility.com>
2026-04-04 17:18:41 +0900

    포인트컷 테스트

--- src/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
+++ src/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
@@ -24,4 +24,44 @@ public class PointcutExpressionTest {
                         && pointcut.getMethodMatcher().matches(Target.class.getMethod("method"), null)
                 , CoreMatchers.is(false));
     }
+
+    @Test
+    public void pointcut() throws Exception{
+        targetClassPointcutMatches("execution(* *(..))", true, true, false, false, false, false);
+        targetClassPointcutMatches("execution(* hello(..))", true, false, false, false, false, false);
+        targetClassPointcutMatches("execution(* hello())", false, true, false, false, false, false);
+        targetClassPointcutMatches("execution(* hello(String))", false, false, false, false, true, true);
+        targetClassPointcutMatches("execution(* meth*(..))", false, false, true, true, false, false);
+        targetClassPointcutMatches("execution(* *(int, int))", true, false, false, false, true, true);
+        targetClassPointcutMatches("execution(* springbook.learningtest.spring.pointcut.Target.*(..))", true, true, true, true, true, false);
+        targetClassPointcutMatches("execution(* springbook.learningtest.spring.pointcut.*.*(..))", true, true, true, true, true, true);
+        targetClassPointcutMatches("execution(* springbook.learningtest.spring.pointcut..*.*(..))", true, true, true, true, true, true);
+        targetClassPointcutMatches("execution(* springbook..*.*(..))", true, true, true, true, true, true);
+        targetClassPointcutMatches("execution(* com..*.*(..))", false, false, false, false, false, false);
+        targetClassPointcutMatches("execution(* *..Target.*(..))", true, true, true, true, true, false);
+        targetClassPointcutMatches("execution(* *..Tar*.*(..))", true, true, true, true, true, false);
+        targetClassPointcutMatches("execution(* *..*get.*(..))", true, true, true, true, true, false);
+        targetClassPointcutMatches("execution(* *..B*.*(..))", false, false, false, false, false, true);
+        targetClassPointcutMatches("execution(* *(..) throws Runtime*)", false, false, false, true, false, true);
+        targetClassPointcutMatches("execution(* *..TargetInterface.*(..))", true, true, true, true, false, false);
+        targetClassPointcutMatches("execution(int *(..))", false, false, true, true, false, false);
+        targetClassPointcutMatches("execution(void *(..))", true, true, false, false, true, true);
+    }
+
+    public void targetClassPointcutMatches(String expression, boolean... expected) throws Exception{
+        pointcutMatches(expression, expected[0], Target.class, "hello");
+        pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+        pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+        pointcutMatches(expression, expected[3], Target.class, "minus");
+        pointcutMatches(expression, expected[4], Target.class, "method");
+        pointcutMatches(expression, expected[5], Bean.class, "method");
+    }
+    public void pointcutMatches(String expression, Boolean expected, Class<?> clazz, String methodName, Class<?>... args) throws Exception{
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(expression);
+
+        Assert.assertThat(pointcut.getClassFilter().matches(clazz)
+                        && pointcut.getMethodMatcher().matches(clazz.getMethod(methodName, args), null)
+                , CoreMatchers.is(expected));
+    }
 }
