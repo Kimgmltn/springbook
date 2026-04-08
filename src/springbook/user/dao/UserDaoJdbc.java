fe62fff4c7511081913c7f2d87129c3a1e535023
khs <T1900843@partner.tmapmobility.com>
2026-04-08 17:55:36 +0900

    sql 서비스 인터페이스를 활용해 sql 생성 관심을 다른 서비스로 이동

--- src/springbook/user/dao/UserDaoJdbc.java
+++ src/springbook/user/dao/UserDaoJdbc.java
@@ -4,6 +4,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 import springbook.user.exceptions.DuplicateUserIdException;
+import springbook.user.sqlService.SqlService;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
@@ -13,10 +14,10 @@ import java.util.Map;
 
 public class UserDaoJdbc implements UserDao{
 
-    private Map<String, Object> sqlMap;
+    private SqlService sqlService;
 
-    public void setSqlMap(Map<String, Object> sqlMap) {
-        this.sqlMap = sqlMap;
+    public void setSqlService(SqlService sqlService) {
+        this.sqlService = sqlService;
     }
 
     private JdbcTemplate jdbcTemplate;  //spring에서 제공하는 jdbcTemplate사용
@@ -43,32 +44,32 @@ public class UserDaoJdbc implements UserDao{
     }
 
     public void add(User user) throws DuplicateUserIdException {
-        this.jdbcTemplate.update((String) this.sqlMap.get("add"),
+        this.jdbcTemplate.update(this.sqlService.getSql("userAdd"),
                 user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail());
     }
 
     public User get(String id){
-        return this.jdbcTemplate.queryForObject((String) this.sqlMap.get("get"),
+        return this.jdbcTemplate.queryForObject(this.sqlService.getSql("userGet"),
                 new Object[] {id},
                 userMapper);
     }
 
     public int getCount(){
-        return this.jdbcTemplate.queryForInt((String) this.sqlMap.get("getCount"));
+        return this.jdbcTemplate.queryForInt(this.sqlService.getSql("userGetCount"));
     }
 
     public void deleteAll(){
-        this.jdbcTemplate.update((String) this.sqlMap.get("deleteAll"));
+        this.jdbcTemplate.update(this.sqlService.getSql("userDeleteAll"));
     }
 
     public List<User> getAll() {
-        return this.jdbcTemplate.query((String) this.sqlMap.get("getAll"),
+        return this.jdbcTemplate.query(this.sqlService.getSql("userGetAll"),
                 userMapper);
     }
 
     @Override
     public void update(User user) {
-        this.jdbcTemplate.update((String) this.sqlMap.get("update"),
+        this.jdbcTemplate.update(this.sqlService.getSql("userUpdate"),
                 user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail(), user.getId());
     }
 }
